574e0e4aee839d467bcb5ff8bf18faf57fb1f9b9
githubfoam <lequestionis@gmail.com>
2020-10-24 22:25:53 +0300

    javadisabled

--- provisioning/bootstrap_elk-master.sh
+++ provisioning/bootstrap_elk-master.sh
@@ -24,72 +24,75 @@ echo "                 ||----w |                                                
 echo "                 ||     ||                                                         "
 echo "========================================================================================="
 
-# Java is required for the Elastic stack deployment.
+
 apt-get install -qqy software-properties-common apt-transport-https
-add-apt-repository ppa:webupd8team/java -y
-apt-get install -qqy oracle-java8-installer
-java -version
-# Check the java binary file 
-update-alternatives --config java
-ls /usr/lib/jvm/java-8-oracle
 
-# create the profile file 'java.sh' under the 'profile.d' directory
-cat <<EOT | sudo tee /etc/profile.d/java.sh
-#Set JAVA_HOME
-JAVA_HOME="/usr/lib/jvm/java-8-oracle"
-export JAVA_HOME
-PATH=$PATH:$JAVA_HOME
-export PATH
-EOT
-cat /etc/profile.d/java.sh 
+# Java is required for the Elastic stack deployment.
+# The repository 'http://ppa.launchpad.net/webupd8team/java/ubuntu focal Release' does not have a Release file
+# add-apt-repository ppa:webupd8team/java -y
+# apt-get install -qqy oracle-java8-installer
+# java -version
+# # Check the java binary file 
+# update-alternatives --config java
+# ls /usr/lib/jvm/java-8-oracle
 
-# Make the file executable and load the configuration file
-chmod +x /etc/profile.d/java.sh
-source /etc/profile.d/java.sh
+# # create the profile file 'java.sh' under the 'profile.d' directory
+# cat <<EOT | sudo tee /etc/profile.d/java.sh
+# #Set JAVA_HOME
+# JAVA_HOME="/usr/lib/jvm/java-8-oracle"
+# export JAVA_HOME
+# PATH=$PATH:$JAVA_HOME
+# export PATH
+# EOT
+# cat /etc/profile.d/java.sh 
 
-# check the java environment
-echo $JAVA_HOME
+# # Make the file executable and load the configuration file
+# chmod +x /etc/profile.d/java.sh
+# source /etc/profile.d/java.sh
 
-# Install Elasticsearch
-# Add the elastic stack key and add the elastic repository to the system.
-wget -qO - https://artifacts.elastic.co/GPG-KEY-elasticsearch | sudo apt-key add -
-echo "deb https://artifacts.elastic.co/packages/6.x/apt stable main" | sudo tee -a /etc/apt/sources.list.d/elastic-6.x.list
+# # check the java environment
+# echo $JAVA_HOME
 
-# update the repository and install the elasticsearch package
-apt-get update -qq && apt-get install -yqq elasticsearch 
+# # Install Elasticsearch
+# # Add the elastic stack key and add the elastic repository to the system.
+# wget -qO - https://artifacts.elastic.co/GPG-KEY-elasticsearch | sudo apt-key add -
+# echo "deb https://artifacts.elastic.co/packages/6.x/apt stable main" | sudo tee -a /etc/apt/sources.list.d/elastic-6.x.list
 
-# Uncomment the 'network.host' line and change the value to 'localhost', 
-# and uncomment the 'http.port' line for the elasticsearch port configuration.
-# network.host: localhost
-# http.port: 9200
-cat /etc/elasticsearch/elasticsearch.yml
-# vim elasticsearch.yml
+# # update the repository and install the elasticsearch package
+# apt-get update -qq && apt-get install -yqq elasticsearch 
 
-systemctl start elasticsearch
-systemctl enable elasticsearch
-systemctl status elasticsearch
+# # Uncomment the 'network.host' line and change the value to 'localhost', 
+# # and uncomment the 'http.port' line for the elasticsearch port configuration.
+# # network.host: localhost
+# # http.port: 9200
+# cat /etc/elasticsearch/elasticsearch.yml
+# # vim elasticsearch.yml
 
-netstat -plntu
-curl -XGET 'localhost:9200/?pretty'
+# systemctl start elasticsearch
+# systemctl enable elasticsearch
+# systemctl status elasticsearch
 
-# Install and Configure Kibana Dashboard
-apt-get install -yqq kibana 
+# netstat -plntu
+# curl -XGET 'localhost:9200/?pretty'
 
-# cd /etc/kibana/
-# vim kibana.yml
-# Uncomment those lines 'server.port', 'server.host', and 'elasticsearch.url'.
-# server.port: 5601
-# server.host: "localhost"
-# elasticsearch.url: "http://localhost:9200"
+# # Install and Configure Kibana Dashboard
+# apt-get install -yqq kibana 
 
-systemctl start kibana
-systemctl enable kibana
-systemctl status kibana
+# # cd /etc/kibana/
+# # vim kibana.yml
+# # Uncomment those lines 'server.port', 'server.host', and 'elasticsearch.url'.
+# # server.port: 5601
+# # server.host: "localhost"
+# # elasticsearch.url: "http://localhost:9200"
 
-# The kibana dashboard is now up and running on the 'localhost' address and the default port '5601'
-netstat -plntu
+# systemctl start kibana
+# systemctl enable kibana
+# systemctl status kibana
 
-# Install and Configure Nginx as Reverse-Proxy for Kibana
-# the Nginx web server as a reverse proxy for the Kibana Dashboard.
+# # The kibana dashboard is now up and running on the 'localhost' address and the default port '5601'
+# netstat -plntu
 
-apt-get install -yqq nginx apache2-utils 
+# # Install and Configure Nginx as Reverse-Proxy for Kibana
+# # the Nginx web server as a reverse proxy for the Kibana Dashboard.
+
+# apt-get install -yqq nginx apache2-utils 
